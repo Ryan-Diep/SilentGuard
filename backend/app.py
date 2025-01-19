bc5ee4e27e323d5c03b226803cff94d4bd3a39c0
Kevin Wu <kevwuhoo15@gmail.com>
2025-01-19 01:44:58 -0500

    setup response subscriber for backend

--- backend/app.py
+++ backend/app.py
@@ -7,48 +7,53 @@ import json
 
 app = Flask(__name__)
 
+# Callback on connection
+def on_connect(client, userdata, flags, rc):
+    print(f'Connected (Result: {rc})')
+    client.subscribe('response-topic')
+
+# Callback when message is sent
+def on_publish(client, userdata, mid):
+    print(f'Sent message (Result: {mid})')
+
+# Callback when message is received from
+def on_message(client, userdata, message):
+    print(f'Received message {message.payload}')
+
+# Function to publish message
+def send_message(client, location):
+    topic = os.getenv('TOPIC')
+    client.publish(topic, location, qos=1)
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+client.on_publish = on_publish
+client.on_message = on_message
+
+# Required if using TLS endpoint (mqtts, wss, ssl), remove if using plaintext
+# Use Mozilla's CA bundle
+client.tls_set(ca_certs=certifi.where())
+
+# Enter your password here
+# Get the parent directory of the current file
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+# Build the path to the .env file
+dotenv_path = os.path.join(parent_dir, ".env")
+load_dotenv(dotenv_path)
+client.username_pw_set('solace-cloud-client', os.getenv('PASSWORD'))
+
+# Use the host and port from Solace Cloud without the protocol
+client.connect(os.getenv('SOLACE_URL'), int(os.getenv('PORT')))
+client.loop_start()
+
 @app.route("/")
 def home():
     return "Hello World Flask"
 
 @app.route("/dummy")
 def dummy():
-    send_message("Ottawa")
+    send_message(client, "Ottawa")
+    # client.on_message = on_message
     return "dummy route works"
-
-# Callback on connection
-def on_connect(client, userdata, flags, rc):
-    print(f'Connected (Result: {rc})')
-
-# Callback when message is sent
-def on_publish(client, userdata, mid):
-    print(f'Sent message (Result: {mid})')
-
-def send_message(location):
-    # If using websockets (protocol is ws or wss), must set the transport for the client as below
-    client = mqtt.Client()
-
-    client.on_connect = on_connect
-    client.on_publish = on_publish
-
-    # Required if using TLS endpoint (mqtts, wss, ssl), remove if using plaintext
-    # Use Mozilla's CA bundle
-    client.tls_set(ca_certs=certifi.where())
-
-    # Enter your password here
-    # Get the parent directory of the current file
-    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-
-    # Build the path to the .env file
-    dotenv_path = os.path.join(parent_dir, ".env")
-    load_dotenv(dotenv_path)
-    client.username_pw_set('solace-cloud-client', os.getenv('PASSWORD'))
-
-    # Use the host and port from Solace Cloud without the protocol
-    client.connect(os.getenv('SOLACE_URL'), int(os.getenv('PORT')))
-
-    client.loop_start()
-    topic = os.getenv('TOPIC')
-    client.publish(topic, location, qos=1)
-    client.loop_stop()
-    client.disconnect()
